db246d73517ef81fa6b08faae8cb576bbafb542d
Luc Heydel <luc@smartb.city>
2022-07-07 18:56:57 +0200

    feat(http client): add logs on call

--- im-commons/im-commons-domain/src/commonMain/kotlin/city/smartb/im/commons/http/Client.kt
+++ im-commons/im-commons-domain/src/commonMain/kotlin/city/smartb/im/commons/http/Client.kt
@@ -20,30 +20,35 @@ open class Client(
     protected val client = httpClientBuilder.build()
 
     protected suspend inline fun <reified T> get(path: String, withAuth: Boolean = true): T {
+        println("GET $path")
         return client.get {
             basicSetup(path, withAuth)
         }.body()
     }
 
     protected suspend inline fun <reified T> post(path: String, jsonBody: Any, withAuth: Boolean = true): T {
+        println("POST json $path")
         return client.post {
             jsonSetup(path, jsonBody, withAuth)
         }.body()
     }
 
     protected suspend inline fun <reified T> post(path: String, formData: Map<String, String>, withAuth: Boolean = true): T {
+        println("POST formdata $path")
         return client.post {
             formDataSetup(path, formData, withAuth)
         }.body()
     }
 
     protected suspend inline fun <reified T> put(path: String, jsonBody: Any, withAuth: Boolean = true): T {
+        println("PUT json $path")
         return client.put {
             jsonSetup(path, jsonBody, withAuth)
         }.body()
     }
 
     protected suspend inline fun <reified T> put(path: String, formData: Map<String, String>, withAuth: Boolean = true): T {
+        println("PUT formdata $path")
         return client.put {
             formDataSetup(path, formData, withAuth)
         }.body()
