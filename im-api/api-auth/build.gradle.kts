6fe2f7ab7eaf3ff488d110a03cc0c12c2f966926
Luc Heydel <luc@smartb.city>
2022-07-27 15:54:19 +0200

    chore(lib): add api-auth and api-config to publication

--- im-api/api-auth/build.gradle.kts
+++ im-api/api-auth/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     id("city.smartb.fixers.gradle.kotlin.jvm")
+    id("city.smartb.fixers.gradle.publish")
     kotlin("plugin.spring")
 }
 
 dependencies {
     implementation("city.smartb.i2:keycloak-auth-domain:${Versions.i2}")
     api("city.smartb.i2:i2-spring-boot-starter-auth:${Versions.i2}")
-
-    // to check if really needed
-//    implementation("city.smartb.f2:f2-spring-boot-starter-function-http:${Versions.f2}")
-//    implementation("org.springframework.boot:spring-boot-starter-webflux:${Versions.springBoot}")
 }
