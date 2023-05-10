5e0c2e8ec579308f38fc336d9981c600dc9bf582
Adrien Poupard <adrien@smartb.city>
2023-05-10 09:09:22 +0200

    Update version to experimental-SNAPSHOT

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ import city.smartb.gradle.dependencies.add
 
 object Framework {
 	val fixers = FixersPluginVersions.fixers
-	val connect = "0.14.0"
+	val connect = "experimental-SNAPSHOT"
 }
 
 object PluginVersions {
